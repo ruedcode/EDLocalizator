62751528f0216a1340773e282cc399d226c2b7d0
Eugene Kalyada <Evgeniy.Kalyada@waveaccess.ru>
2017-09-06 13:05:55 +0500

    first commit

---
+++ EDLocalizator/EDLocalizator.h
@@ -0,0 +1,19 @@
+//
+//  EDLocalizator.h
+//  EDLocalizator
+//
+//  Created by Eugene Kalyada on 06.09.17.
+//  Copyright © 2017 Eugene Kalyada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EDLocalizator.
+FOUNDATION_EXPORT double EDLocalizatorVersionNumber;
+
+//! Project version string for EDLocalizator.
+FOUNDATION_EXPORT const unsigned char EDLocalizatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDLocalizator/PublicHeader.h>
+
+
---
+++ EDLocalizator/Localizator.swift
@@ -0,0 +1,84 @@
+//
+//  Localizator.swift
+//  EDLocalizator
+//
+//  Created by Eugene Kalyada on 06.09.17.
+//  Copyright © 2017 Eugene Kalyada. All rights reserved.
+//
+
+import UIKit
+
+private class Localizator {
+
+	static let sharedInstance = Localizator()
+	var resourcePath : String = "Localizable"
+
+	lazy var localizableDictionary: NSDictionary! = {
+		if let path = Bundle.main.path(forResource: self.resourcePath, ofType: "plist") {
+			return NSDictionary(contentsOfFile: path)
+		}
+		fatalError("Localizable file NOT found")
+	}()
+
+	func localize(string: String) -> String {
+		guard var localizedString = localizableDictionary.value(forKeyPath:string) as? String else {
+			print("Missing translation for: \(string)")
+			return string
+		}
+		localizedString.isLocalized = true
+		return localizedString
+	}
+
+}
+protocol PropertyStoring {
+
+	associatedtype T
+
+	func getAssociatedObject(_ key: UnsafeRawPointer!, defaultValue: T) -> T
+}
+
+extension PropertyStoring {
+	func getAssociatedObject(_ key: UnsafeRawPointer!, defaultValue: T) -> T {
+		guard let value = objc_getAssociatedObject(self, key) as? T else {
+			return defaultValue
+		}
+		return value
+	}
+}
+
+
+struct AssociatedKeys {
+	static var isLocalized: UInt8 = 0
+}
+
+extension String : PropertyStoring {
+
+	typealias T = Bool
+
+	private struct CustomProperties {
+		static var isLocalized = false
+	}
+
+	var isLocalized: Bool {
+		get {
+			return getAssociatedObject(&CustomProperties.isLocalized, defaultValue: CustomProperties.isLocalized)
+		}
+		set {
+			return objc_setAssociatedObject(self, &CustomProperties.isLocalized, newValue, .OBJC_ASSOCIATION_RETAIN)
+		}
+	}
+
+	var localized : String {
+		get {
+			if !isLocalized {
+				return Localizator.sharedInstance.localize(string: self)
+			}
+			return self
+		}
+	}
+
+	func localized(_ args:CVarArg...) -> String {
+
+		return String(format: self.localized, arguments:args)
+	}
+}
